e09c3ec6ed6b583d9bb31afb3d89b8523f7fbcab
Martin Schwarzer <polyluxus@gmail.com>
2019-01-08 22:20:12 +0100

    Bugfix: Proper RegEx so that shellcheck is not confused. Enhanced: Update script checks with git diff whether the directory has changed compared to upstream.

--- meta/update-version.sh
+++ meta/update-version.sh
@@ -2,21 +2,29 @@
 
 update_file ()
 {
-  [[ "$1" =~ \* ]] && return
+  [[ "$1" =~ [\*]+ ]] && return
   printf 'Updating "%s" ... ' "$1"
   sed -i "s/___version___: [[:digit:]]\\{4\\}-[[:digit:]]\\{2\\}-[[:digit:]]\\{2\\}-[[:digit:]]\\{4\\}/___version___: $insert_version/" "$1"
   printf 'done.\n'
 }
 
-git_root=$(git rev-parse --show-toplevel)
+git fetch
+git_root=$( git rev-parse --show-toplevel )
+git_branch=$( git rev-parse --abbrev-ref HEAD )
 insert_version=$( date '+%Y-%m-%d-%H%M' )
 
 for directory in "$git_root" "${git_root}"/* ; do
   [[ -d $directory ]] || continue
-  pushd "$directory" || { echo "ERROR changing directory" ; exit 1 ; }
-  for file in *.sh *.bash *.md *.markdown ; do
-    update_file "$file"
-  done
-  popd &> /dev/null || { echo "ERROR changing directory" ; exit 1 ; }
+  does_differ=$( git diff "origin/$git_branch" -- "$directory" )
+  if [[ -n $does_differ ]] ; then
+    pushd "$directory" || { echo "ERROR changing directory" ; exit 1 ; }
+    for file in *.sh *.bash *.md *.markdown ; do
+      [[ "$file" =~ [\*]+ ]] && continue 
+      update_file "$file"
+    done
+    popd &> /dev/null || { echo "ERROR changing directory" ; exit 1 ; }
+  else
+    printf 'Unchanged: %s.' "$directory"
+  fi
 done
 
