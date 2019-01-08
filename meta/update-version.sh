4648880211b6fd2969001111ce7bbdc98a8e19c8
Martin Schwarzer <polyluxus@gmail.com>
2019-01-08 21:45:45 +0100

    Bugfix: Fine-tuning the version updater

--- meta/update-version.sh
+++ meta/update-version.sh
@@ -2,6 +2,7 @@
 
 update_file ()
 {
+  [[ "$1" =~ \* ]] && return
   printf 'Updating "%s" ... ' "$1"
   sed -i "s/___version___: [[:digit:]]\\{4\\}-[[:digit:]]\\{2\\}-[[:digit:]]\\{2\\}-[[:digit:]]\\{4\\}/___version___: $insert_version/" "$1"
   printf 'done.\n'
@@ -10,11 +11,12 @@ update_file ()
 git_root=$(git rev-parse --show-toplevel)
 insert_version=$( date '+%Y-%m-%d-%H%M' )
 
-for directory in "$git_root" "${git_root}/*" ; do
+for directory in "$git_root" "${git_root}"/* ; do
+  [[ -d $directory ]] || continue
   pushd "$directory" || { echo "ERROR changing directory" ; exit 1 ; }
   for file in *.sh *.bash *.md *.markdown ; do
     update_file "$file"
   done
-  popd || { echo "ERROR changing directory" ; exit 1 ; }
+  popd &> /dev/null || { echo "ERROR changing directory" ; exit 1 ; }
 done
 
