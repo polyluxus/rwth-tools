79abf6c9c4b12b029ca469aafe3bef77e73457d8
Martin Schwarzer <polyluxus@gmail.com>
2019-01-08 20:49:59 +0100

    adding module initialising script which could (and should) have been a replacement for the currently used one on the rwth cluster

---
+++ init-modules/README.md
@@ -0,0 +1,8 @@
+# init-modules.sh
+
+This is a small, updated replacement script for the module command as it is used currently.
+
+According to the IT service desk, a new implementation of the module system is being prepared,
+which makes this script deprecated.
+
+___version___: 2019-01-08-2030
---
+++ init-modules/init-modules.bash
@@ -0,0 +1,24 @@
+#!/bin/env bash
+# Make function 'module' available, emulating the autoload feature; 
+# which is available for ksh and zsh, but not for bash!
+module () {
+  # If root, do nothing, see https://askubuntu.com/a/836092/220129
+  if ! (( ${EUID:-0} || "$(id -u)"  )) ; then 
+    echo "module: not available for root" >&2 
+    return 1 
+  fi
+
+  # Redefine 'module'.
+  local module_cmd="modulecmd.tcl"
+  local module_dir="/usr/local_rwth/modules/src"
+
+  if local -r source_autoinit=$( tclsh "$module_dir/$module_cmd" sh autoinit 2> /dev/null ) ; then
+    $source_autoinit
+    module "$@"
+  else
+    echo "ERROR: no modules available" >&2 
+    return
+  fi
+
+}
+
