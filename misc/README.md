08e9cc0589732320bbc0b07873766460d2b29c77
Martin Schwarzer <polyluxus@gmail.com>
2019-01-17 21:27:32 +0100

    Bugfix: Fixed pattern recognition for isit2late

--- misc/README.md
+++ misc/README.md
@@ -4,3 +4,7 @@ This directory contains scripts which are mainly fun little scripts;
 they don't serve a particular purpose.
 
 ___version___: 2019-01-16-1808
+
+## Resources
+
+- [How can I use a variable as a case condition?](https://unix.stackexchange.com/q/234264/160000)
--- misc/ii2l.bash
+++ misc/ii2l.bash
@@ -1,55 +1,82 @@
 #!/bin/bash
+# It is important to turn on extended globbing
+if shopt -p extglob > /dev/null ; then
+  :
+else
+  shopt -s extglob > /dev/null
+fi
 
 fatal ()
 {
   echo "ERROR: $*"
+  [[ -n $testmode ]] && return 1
   exit 1
 }
 
 isittoolate ()
 {
-  local current color uncolor text
-  current=$(date +%H:%M)
+  local current color uncolor text 
+  current="$1"
   uncolor="$(tput sgr0)"
+  local use_silent use_oklate use_almost2late use_itis2late use_early use_morning use_lunch use_default
+  use_silent="@($__silent)"
+  use_oklate="@($__oklate)"
+  use_almost2late="@($__almost2late)"
+  use_itis2late="@($__itis2late)"
+  use_early="@($__early)"
+  use_morning="@($__morning)"
+  use_lunch="@($__lunch)"
+  use_default="@($__default)"
   exec 3>&1
-  case $current in
-    $__silent)
+  case "$current" in
+    $use_silent)
       exec 3> /dev/null
-      ;;
-    $__oklate)
+      ;;&
+    $use_oklate)
       color="$(tput setf 6)"
       text="It is late: $current."
       ;;
-    $__almost2late)
+    $use_almost2late)
       color="$(tput setf 5)"
       text="It is getting too late: $current."
       ;;
-    $__itis2late)
+    $use_itis2late)
       color="$(tput setf 4)"
       text="It is too late: $current."
       ;;
-    $__early)
+    $use_early)
       color="$(tput setf 3)"
       text="It is quite early: $current."
       ;;
-    $__morning)
+    $use_morning)
       color="$(tput setf 2)"
       text="Good morning! ($current)"
       ;;
-    $__lunch)
+    $use_lunch)
       color="$(tput setf 5)"
       text="It is almost lunch ($current)!"
       ;;
-    $__default)
+    $use_default)
       color="$(tput setf 3)"
       text="Current time: $current."
       ;;
     *)
-      fatal "No rule could be applied."
+      fatal "No rule could be applied." || { exec 3>&- ; return 1 ; }
       ;;
   esac
   printf '%s%s%s\n' "${color}" "${text}" "${uncolor}" >&3
-  exec >&-
+  exec 3>&-
+}
+
+testfunction()
+{
+  local testtime
+  testmode="true"
+  for testtime in {00..23}:{0..5}0 ; do
+    printf '%s  ' "$testtime"
+    isittoolate "$testtime"
+  done
+  exit 0
 }
 
 write_to_file ()
@@ -64,7 +91,12 @@ configure ()
   if command -v vi > /dev/null ; then
     local rcfile="$HOME/.ii2lrc"
     exec 4> "$rcfile"
-    write_to_file  "# This uses globbing like in the case statement."
+    write_to_file  "# This skript ($0) uses globbing like patterns."
+    write_to_file  "# The general format is 'HH:MM'."
+    write_to_file  "# The '*' matches any character, brackets '[]' may be used to group values,"
+    write_to_file  "# a pipe '|' may be used to separate values."
+    write_to_file  "# Example: The pattern '1[123]:*' matches the times 11:00 to 13:59."
+    write_to_file  "# Example: The pattern '0[78]:*|09:[012]*' matches the times 7:00 to 9:29."
     write_to_file  "__silent='$__silent'"
     write_to_file  "__oklate='$__oklate'"
     write_to_file  "__almost2late='$__almost2late'"
@@ -77,7 +109,7 @@ configure ()
     vi "$rcfile"
     exit 0
   else
-    fatal "Command not found: vi"
+    fatal "Command not found: vi" 
   fi
 }
 
@@ -92,6 +124,7 @@ __early="0[678]:*"
 __morning="09:*|10:*"
 __lunch="11:*"
 __default="1[2345678]:*"
+testmode=''
 
 # Allow for configuration file
 [[ -r "$HOME/.ii2lrc" ]] && . "$HOME/.ii2lrc"
@@ -103,15 +136,17 @@ if [[ "$1" == "-h" ]] ; then
   exit 0
 elif [[ "$1" == "-c" ]] ; then
   configure
+elif [[ "$1" == "test" ]] ; then
+  testfunction
 fi
 
 # Checks
 if command -v tput > /dev/null ; then
   :
 else
-  fatal "Command not found: tput"
+  fatal "Command not found: tput" 
 fi
 
 # Execution
-isittoolate 
+isittoolate "$(date +%H:%M)" 
 
