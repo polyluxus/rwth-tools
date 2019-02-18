e7b0bdb0d87675ab64fcf6c27c209c5ca8b22264
Martin Schwarzer <polyluxus@gmail.com>
2019-02-18 19:07:07 +0100

    Added: Add initialisation script for shell completition for tldr in dotfiles directory

---
+++ dotfiles/bash_profile.d/init_tldr.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#___version___: 2019-02-00-0000
+
+if (( ${#BASH_SOURCE[*]} == 1 )) ; then
+  echo "This script is only meant to be sourced."
+  exit 0
+fi
+
+# Check if tldr is installed
+# add it to bash completition
+if command -v tldr &> /dev/null ; then
+  complete -W "$(tldr 2>/dev/null --list)" tldr
+fi
+#
+
