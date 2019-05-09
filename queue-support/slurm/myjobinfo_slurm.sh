af439bc18a1a093b9609f3b985c96d8385b4af6a
Martin Schwarzer <polyluxus@gmail.com>
2019-05-09 18:23:41 +0200

    Extended: slurm support scripts, config. files
    implemented (more) options for the slurm support scripts
    mail script can now access 'seff' (with switch to explicitly turn off seff output)
    scripts can be configured via rc file

--- queue-support/slurm/myjobinfo_slurm.sh
+++ queue-support/slurm/myjobinfo_slurm.sh
@@ -7,22 +7,42 @@ else
   exit 1
 fi
 
+# set defaults
 sacct_format_default="JobID,JobName,WorkDir,Cluster,User,Group,Account,Partition,State,ExitCode,"
-sacct_format_default+="AllocCPU,AllocNodes,CPUTime,CPUTimeRAW,Elapsed,ElapsedRaw,"
+sacct_format_default+="AllocCPU,AllocNodes,CPUTime,CPUTimeRAW,Elapsed,ElapsedRaw,ReqMEM,MaxRSS,"
 sacct_format_default+="Submit,Start,End"
-declare -- only_two
+only_two="false"
+include_seff="false"
 
 #hlp The script is an interface to the sacct command to reformat the output.
 #hlp 
 #hlp Usage:
 #hlp   ${0##*/} [option(s)] jobid
-#hlp   
+#hlp
+
+#hlp Configuration files:
+#hlp   '/etc/myslurm.rc'          (loaded first)
+#hlp   '$HOME/.myslurmrc' 
+#hlp   '$HOME/.config/myslurm.rc' (loaded last, i.e. superior)
+#hlp
+
+# Source a global configuration
+#shellcheck source=myslurm.rc
+[[ -r "/etc/myslurm.rc" ]] && . "/etc/myslurm.rc"
+#shellcheck source=myslurm.rc
+[[ -r "$HOME/.myslurmrc" ]] && . "$HOME/.myslurmrc"
+#shellcheck source=myslurm.rc
+[[ -r "$HOME/.config/myslurm.rc" ]] && . "$HOME/.config/myslurm.rc"
+
+# Declare default seff command:
+seff_cmd="${seff_cmd:-seff}"
+
 OPTIND=1
 
-while getopts :f:Fmh options ; do
+while getopts :f:Fmeh options ; do
   #hlp Options:
   case $options in
-    #hlp      -f           Show different fields than coded within this script.
+    #hlp      -f <ARG>     Show different fields than coded within this script.
     #hlp                   (default: $sacct_format_default)
     #hlp
     f)
@@ -40,6 +60,20 @@ while getopts :f:Fmh options ; do
     #hlp
     m)
       only_two=true
+      ;;
+
+    #hlp      -e           Include the output of the efficiency script (seff, if installed)
+    #hlp                   (default: $include_seff)
+    #hlp
+    e)
+      include_seff="true"
+      ;;
+
+    #hlp      -E           Exclude the output of the efficiency script (seff)
+    #hlp                   (Overwrite configuration settings and -e switch.)
+    #hlp
+    e)
+      include_seff="true"
       ;;
 
     #hlp      -h           Show this help file and exit.  
@@ -74,6 +108,11 @@ while [[ -n $1 ]] ; do
   shift
 done
 
+# the part about seff goes here
+if [[ "$include_seff" == "true" ]] ; then
+  command -v "$seff_cmd" &> /dev/null && "$seff_cmd" "$show_jobid"
+fi
+
 sacct_format="${sacct_format:-$sacct_format_default}"
 while read -r line || [[ -n $line ]] ; do
 #  echo "Read: $line"
@@ -88,6 +127,6 @@ while read -r line || [[ -n $line ]] ; do
     printf '%-20s: %s\n' "${header[index]}" "${body[index]}"
   done
   printf '==================================================\n\n'
-  [[ -n $only_two ]] && break # breakout
+  [[ "$only_two" == "true" ]] && break # breakout
 done < <( sacct --jobs="$show_jobid" --parsable --format="$sacct_format" )
 
